43bc5ea5ee57038b2cbaa857738b6cef1b0e539f
thisisartorius <artur.poe@gmail.com>
2021-09-06 11:48:53 -0400

    Update PopupWithImage.js

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,14 +1,18 @@
 import Popup from "./Popup";
 
 class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupCardImage = this._popupElement.querySelector(".popup__image");
+    this._captionImage = this._popupElement.querySelector(".popup__preview-title");
+  }
+  
   open({ name, link }) {
     super.open();
 
-    const image = this._popupElement.querySelector(".popup__image");
-    const text = this._popupElement.querySelector(".popup__preview-title");
-    image.src = link;
-    image.alt = name;
-    text.textContent = name;
+    this._popupCardImage.src = link;
+    this._popupCardImage.alt = name;
+    this._captionImage.textContent = name;
   }
 }
 
